be61a9c0e2644512a59378c082aaf1855ea7527e
Galobtter <galobtter@gmail.com>
2019-12-07 00:27:25 -0800

    Allow command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 use is_prime::is_prime;
+use std::env;
 use std::io;
+use std::num::ParseIntError;
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() >= 2 {
+        let _ = check_str_prime(&args[1]);
+    } else {
+        interactive_loop();
+    }
+}
+
+fn interactive_loop() {
     loop {
         println!("Input number for checking if it is a prime.");
 
@@ -9,18 +20,25 @@ fn main() {
         io::stdin()
             .read_line(&mut num)
             .expect("Failed to read line");
-        match num.trim().parse() {
-            Ok(n) => {
-                if is_prime(n) {
-                    println!("This is a prime.");
-                } else {
-                    println!("This is not a prime.");
-                };
-            }
-            Err(_) => {
-                println!("Input could not be parsed as a number.");
-                continue;
-            }
+        if check_str_prime(&num).is_err() {
+            continue;
+        }
+    }
+}
+
+fn check_str_prime(num: &str) -> Result<u64, ParseIntError> {
+    match num.trim().parse() {
+        Ok(n) => {
+            if is_prime(n) {
+                println!("This is a prime.");
+            } else {
+                println!("This is not a prime.");
+            };
+            return Ok(n);
+        }
+        Err(e) => {
+            println!("Input could not be parsed as a number.");
+            return Err(e);
         }
     }
 }
