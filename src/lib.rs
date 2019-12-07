8a11b0f107aff5fcd29a9974c89e107b685dc99c
Galobtter <galobtter@gmail.com>
2019-12-07 15:33:48 -0800

    More tests

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,17 @@ use std::cmp::min;
 use std::thread;
 static NUM_THREADS: u64 = 4;
 pub fn is_prime(num: u64) -> bool {
+    is_prime_internal(num, false)
+}
+
+fn is_prime_internal(num: u64, no_threads: bool) -> bool {
     if num == 1 || num % 2 == 0 && num != 2 || num % 3 == 0 && num != 3 {
         return false;
+    } else if num == 5 || num == 7 {
+        return true;
     }
     let max = integer_sqrt(num) / 6 + 1;
-    if num < 65536 {
+    if no_threads || num < 65536 {
         return check_prime(1, max, num);
     } else {
         return threaded_is_prime(num, max);
@@ -47,6 +53,36 @@ fn integer_sqrt(num: u64) -> u64 {
             return l;
         } else {
             return s;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    extern crate rand;
+    use rand::Rng;
+    fn simple_prime_check(num: u64) -> bool {
+        !(2..num).any(|n| num % n == 0)
+    }
+    #[test]
+    fn correct_primality_simple_check() {
+        for n in 2..u64::pow(2, 16) {
+            assert_eq!(simple_prime_check(n), is_prime(n), "{}", n)
+        }
+    }
+    #[test]
+    fn correct_primality_threaded_check() {
+        let mut rng = rand::thread_rng();
+        for p in 1..u16::pow(2, 16) {
+            println!("{}", p);
+            let n: u32 = rng.gen();
+            assert_eq!(
+                is_prime_internal(n as u64, true),
+                is_prime(n as u64),
+                "{}",
+                n
+            )
         }
     }
 }
